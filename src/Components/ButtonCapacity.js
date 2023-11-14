d3a7d3763c3a23315436fdde201941ded55cc12f
Amayas-Tamarat <amayas.tamarat@gmail.com>
2023-11-14 15:30:27 +0100

    function

--- src/Components/ButtonCapacity.js
+++ src/Components/ButtonCapacity.js
@@ -1,23 +1,49 @@
+// import React from 'react';
+
+
+
+// class ButtonCapacity extends React.Component {
+//      combat = () => {
+//         console.log('aie !')
+//     }
+//     render() {
+//         return (
+//             <button type="button" onClick={() => this.combat()} className="btn btn-success material-tooltip-main ">
+//                 hit
+//             <i className="fas fa-bomb"></i> 5
+//         <i className="fas fa-fire-alt"></i> - 5
+//     </button>
+//         )
+//     }
+
+// }
+ 
+
 import React from 'react';
 
-
-
-class ButtonCapacity extends React.Component {
-     combat = () => {
-        console.log('aie !')
-    }
-    render() {
-        return (
-            <button type="button" onClick={() => this.combat()} className="btn btn-success material-tooltip-main ">
-                hit
-            <i className="fas fa-bomb"></i> 5
-        <i className="fas fa-fire-alt"></i> - 5
-    </button>
-        )
-    }
-
+function combat() {
+    console.log('aie !');
 }
 
-
+const ButtonCapacity = () => {
+    return (
+        <button
+            type="button"
+            onClick={combat}
+            className="btn btn-success material-tooltip-main"
+        >
+            hit
+            <i className="fas fa-bomb"></i> 5
+            <i className="fas fa-fire-alt"></i> - 5
+        </button>
+    );
+};
 
 export default ButtonCapacity;
+
+
+
+
+
+
+// export default ButtonCapacity;
