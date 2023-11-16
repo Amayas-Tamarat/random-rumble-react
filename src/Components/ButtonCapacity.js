c2127c0b63d37df3118ae7122b724ec5836e94e4
Amayas-Tamarat <amayas.tamarat@gmail.com>
2023-11-16 11:54:29 +0100

    turn system

--- src/Components/ButtonCapacity.js
+++ src/Components/ButtonCapacity.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { hitBack, hitMonster } from "../features/fight/fightSlice";
+import { hitBack, hitMonster, togglePlayerTurn, resetPlayerTurns} from "../features/fight/fightSlice";
 
 const ButtonCapacity = ({ playerId }) => {
   const dispatch = useDispatch();
   const player = useSelector((state) => state.fight.players[playerId - 1]);
+  const playerTurn = player.turn;
 
   const combat = () => {
     dispatch(hitMonster({ 
@@ -14,6 +15,8 @@ const ButtonCapacity = ({ playerId }) => {
         playerHit: playerId-1,
         playerDecreasePv: 5
     }))
+    dispatch(togglePlayerTurn(playerId - 1));
+    dispatch(resetPlayerTurns());
   };
 
   return (
@@ -21,8 +24,7 @@ const ButtonCapacity = ({ playerId }) => {
       type="button"
       onClick={() => combat()}
       className="btn btn-success material-tooltip-main "
-      disabled={player.pv <= 0} 
-    >
+      disabled={player.pv <= 0 || playerTurn}    >
       hit
       <i className="fas fa-bomb"></i> 5<i className="fas fa-fire-alt"></i> - 5
     </button>
--- src/features/fight/fightSlice.js
+++ src/features/fight/fightSlice.js
@@ -1,36 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    players :[
-        { name: "John", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 1 },
-        { name: "Jack", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 2 },
-        { name: "Jessy", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 3 },
-        { name: "Jenny", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 4 },
-    ],
+  players: [
+    { name: "John", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 1, turn: false },
+    { name: "Jack", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 2, turn: false },
+    { name: "Jessy", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 3, turn: false },
+    { name: "Jenny", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 4, turn: false },
+  ],
 
-    monster: {
-        pv : 800,
-        pvMax : 800
-    }
+  monster: {
+    pv: 800,
+    pvMax: 800
+  }
 }
 
 export const fightSlice = createSlice({
-    name: "fight",
-    initialState,
-    reducers: {
-      hitMonster: (state, action) => {
-        state.monster.pv -= action.payload.monsterDecreasePv;
-      },
-      hitBack: (state, action) => {
-        const { playerHit, playerDecreasePv } = action.payload;
-        const playerToUpdate = state.players[playerHit];
-          if (playerToUpdate && playerToUpdate.pv > 0) {
-          playerToUpdate.pv -= playerDecreasePv;
-        }
-      },
+  name: "fight",
+  initialState,
+  reducers: {
+    hitMonster: (state, action) => {
+      state.monster.pv -= action.payload.monsterDecreasePv;
     },
-  });
-  
-  export const { hitMonster, hitBack } = fightSlice.actions
+    hitBack: (state, action) => {
+      const { playerHit, playerDecreasePv } = action.payload;
+      const playerToUpdate = state.players[playerHit];
+      if (playerToUpdate && playerToUpdate.pv > 0) {
+        playerToUpdate.pv -= playerDecreasePv;
+      }
+    },
+    togglePlayerTurn: (state, action) => {
+      const playerId = action.payload;
+      state.players[playerId].turn = !state.players[playerId].turn;
+    },
+    resetPlayerTurns: (state) => {
+      const allPlayersTurn = state.players.every((player) => player.turn === true);
+
+      if (allPlayersTurn) {
+        state.players.forEach((player) => {
+          player.turn = false;
+        });
+      }
+    },
+  },
+});
+
+export const { hitMonster, hitBack, togglePlayerTurn, resetPlayerTurns } = fightSlice.actions
 
 export default fightSlice.reducer
